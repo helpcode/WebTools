24c94e6166c3d90124c6f0b0790648109eda5b16
bmy <2271608011@qq.com>
2017-04-06 03:52:21 +0800

    修改页面切换特效

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
       <footer-component ref="footer"></footer-component>
       <keep-alive>
-          <transition name="slide-fade" v-on:before-enter="beforeEnter">
+          <transition name="bounce" v-on:before-enter="beforeEnter">
             <router-view ref="content"></router-view>
           </transition>
       </keep-alive>
@@ -32,16 +32,33 @@ export default {
     #content-component{
         margin: 55px 0 50px 0;
     }
-    /* 可以设置不同的进入和离开动画 */
-    /* 设置持续时间和动画函数 */
-    .slide-fade-enter-active {
-        transition: all .3s ease;
+
+    .bounce-enter-active {
+        animation: bounce-in .5s;
     }
-    .slide-fade-leave-active {
-        transition: all .6s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    .bounce-leave-active {
+        animation: bounce-out .5s;
     }
-    .slide-fade-enter, .slide-fade-leave-active {
-        transform: translateX(10px);
-        opacity: 0;
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+    @keyframes bounce-out {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(0);
+        }
     }
 </style>
